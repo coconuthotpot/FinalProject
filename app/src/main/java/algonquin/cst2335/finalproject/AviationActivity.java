ead94aca7e1a75774d699533765cea03e43f782d
tuying <tu000021@algonquinlive.com>
2023-07-16 15:42:39 -0400

    Finished 4-1

--- app/src/main/java/algonquin/cst2335/finalproject/AviationActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/AviationActivity.java
@@ -3,6 +3,9 @@ package algonquin.cst2335.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class AviationActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class AviationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aviation);
+
+        Button showToastButton = findViewById(R.id.button);
+
+        showToastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AviationActivity.this, "This is a Toast notification", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
