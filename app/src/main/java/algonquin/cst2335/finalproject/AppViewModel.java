412187144bbe3ee1f8c980bb52ca7feb14ba8c8c
Nic, Ka Yan Ieong <ieon0001@algonquinlive.com>
2023-07-16 14:03:00 -0400

    Added menu page with icons, and viewmodel class for change in device orientation

---
+++ app/src/main/java/algonquin/cst2335/finalproject/AppViewModel.java
@@ -0,0 +1,8 @@
+package algonquin.cst2335.finalproject;
+
+import androidx.lifecycle.ViewModel;
+
+public class AppViewModel extends ViewModel {
+
+    // used to keep data when turning device orientation
+}
