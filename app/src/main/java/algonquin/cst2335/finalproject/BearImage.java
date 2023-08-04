38193076fe5a83160da5c3d0a93db24db97d32e1
Chen <102144862+coconuthotpot@users.noreply.github.com>
2023-08-04 13:58:27 -0400

    Revert "finished final project"

--- app/src/main/java/algonquin/cst2335/finalproject/BearImage.java
+++ app/src/main/java/algonquin/cst2335/finalproject/BearImage.java
@@ -1,99 +1,51 @@
 package algonquin.cst2335.finalproject;
 
-import android.graphics.Bitmap;
-
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-/**
- * this class present the object of bear image
- */
 @Entity
 public class BearImage {
-    /**
-     * the width of the image
-     */
     @ColumnInfo(name="Width")
-    String width;
+    private int width;
 
-    /**
-     * the height of the image
-     */
     @ColumnInfo(name="Height")
-    String height;
+    private int height;
+    @ColumnInfo(name="URL")
+    private String imageUrl;
 
-    /**
-     * the time that image generated
-     */
-    @ColumnInfo(name = "timeGenerated")
-    protected String timeGenerated;
-
-
-    /**
-     *the id of the image
-     */
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name="ID")
     public long id;
 
-    /**
-     *the no arg constructor
-     */
-    public BearImage(){}
-
-    /**
-     * this is the constructor
-     * @param timeGenerated  the time image generater
-     * @param width  the width of image
-     * @param height the height of image
-     */
-    public BearImage(String timeGenerated,String width, String height) {
-        this.timeGenerated= timeGenerated;
+    public BearImage(int width, int height, String imageUrl) {
         this.width = width;
         this.height = height;
-
+        this.imageUrl = imageUrl;
     }
 
-    /**
-     * get the width of the image
-     * @return return the width
-     */
-    public String getWidth() {
+    public int getWidth() {
         return width;
     }
 
-    /**
-     * set the width of image
-     * @param width the width to be setted
-     */
-    public void setWidth(String width) {
+    public void setWidth(int width) {
         this.width = width;
     }
 
-    /**
-     * get the height of the image
-     * @return return the height of the image
-     */
-    public String getHeight() {
+    public int getHeight() {
         return height;
     }
 
-    /**
-     * set the height of the image
-     * @param height return the height of image
-     */
-    public void setHeight(String height) {
+    public void setHeight(int height) {
         this.height = height;
     }
 
+    public String getImageUrl() {
+        return imageUrl;
+    }
 
-    /**
-     * get the time of image generated
-     * @return return the time of image generated
-     */
-    public String getTimeGenerated() {
-        return timeGenerated;
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 
 }
