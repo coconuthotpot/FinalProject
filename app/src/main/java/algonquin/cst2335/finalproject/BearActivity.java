6a8adb8f4413a2b2b1dca1203da2035f0821cafe
Shiriatsu <siriusying716@gmail.com>
2023-08-08 17:05:31 -0400

    Finished flight test1

--- app/src/main/java/algonquin/cst2335/finalproject/BearActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/BearActivity.java
@@ -19,54 +19,7 @@ import algonquin.cst2335.finalproject.databinding.ActivityBearBinding;
 
 public class BearActivity extends AppCompatActivity {
 
-    ActivityBearBinding binding;
 
-    SharedPreferences prefs;
-    SharedPreferences.Editor editor;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ActivityBearBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
-        editor= prefs.edit();
-
-        //prefs.getString("width", "");
-       // prefs.getString("height", "");
-        binding.width.setText(prefs.getString("width", ""));
-        binding.height.setText(prefs.getString("height", ""));
-
-       // SharedPreferences.Editor editor = prefs.edit();
-
-
-        binding.button.setOnClickListener(clk-> {
-
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-            builder.setMessage("Are you sure you want to generate the image?");
-            builder.setPositiveButton("Yes", (dialog, click) -> {
-                String width = binding.width.getText().toString();
-                String height = binding.height.getText().toString();
-
-                Toast.makeText(this, "Image generated!", Toast.LENGTH_SHORT).show();
-
-                Snackbar.make(binding.getRoot(), "Generated image with size: " + width + "*" + height, Snackbar.LENGTH_LONG)
-
-                        .show();
-
-                editor.putString("width", width);
-                editor.putString("height", height);
-                editor.apply();
-                    });
-
-                builder.setNegativeButton("No", (dialog2, click2) -> {});
-                builder.create().show();
-
-
-            });
-        }
     }
 
 
